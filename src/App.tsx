fb6a8c760d29d9b6c4511573477d99137bb017f6
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-03 16:26:10 +0200

    implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,9 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
