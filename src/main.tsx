5f5293541cf21cd38aab3878bb476b2f2416d8e0
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-03 12:22:10 +0200

    build a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import { Provider } from "./components/ui/provider";
-import { Button } from "@chakra-ui/react";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <Button />
       <App />
     </Provider>
   </StrictMode>,
