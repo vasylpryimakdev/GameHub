ffa81275d7a9f4486566b2013edb2dcd93e38f75
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-03 12:31:34 +0200

    build navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import logo from "../assets/logo.webp";
+import { HStack, Image, Text } from "@chakra-ui/react";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>NavBar</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
