b401ad1367662ffa3a33c3b65f79108332551fce
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-03 14:53:29 +0200

    displed genres

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,17 +2,6 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-
-export interface Genre {
-  id: number;
-  name: string;
-}
-
 interface FetchResponse<T> {
   count: number;
   results: T[];
