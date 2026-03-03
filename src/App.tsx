ec7dfb7e4ce2a1e7ef7fdde86e61cf784455d08f
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-03-03 13:04:17 +0200

    build color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -12,10 +12,13 @@ function App() {
       <GridItem area="nav">
         <NavBar />
       </GridItem>
-      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
-        Aside
-      </GridItem>
-      <GridItem area="main" bg="dodgerblue">
+      <Show above="lg">
+        <GridItem area="aside">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main">
         Main
       </GridItem>
     </Grid>
